5d7ff7cefc608ba8508a1203d05ddaf99e9719c9
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-08 20:11:47 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,14 @@
+
+def greeting(name)
+  5.times do 
+    puts "Hi, #{name}"
+  end 
+end
+  
+  puts "What is your name?"
+    name = gets.to_s 
+  puts "My name is BoP"
+  puts "People usually call me: " 
+    2.times do
+      puts "Hey! You!"
+    end 
