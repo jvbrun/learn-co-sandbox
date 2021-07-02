d2dce9b081f8558d1b87603d721c8d4cbd6d9259
Jean-Victor Brun <jeanvbrun@icloud.com>
2021-07-02 13:33:49 +0000

    Automatically backed up by Learn

--- say_anyting_x_times.rb
+++ say_anyting_x_times.rb
@@ -5,3 +5,23 @@ def say_anything_x_times (phrase, x)
 end
 
 say_anything_x_times("hello world", 5)
+
+
+array.sort do |a, b|
+  if a == b 
+    0 
+  elsif a < b 
+    -1
+  elsif a > b 
+    1 
+  end
+end 
+
+array.each do |num|
+  puts num 
+end
+
+
+array.sort do |a, b|
+  a <=> b 
+end
